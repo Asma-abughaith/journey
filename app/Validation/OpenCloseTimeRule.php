d372a541f7406a8e96cef719e8025c4791f3bec7
Asma Abughaith <asma.abughaith@gmail.com>
2024-02-23 20:23:48 +0300

    crached rule for opening and closing hours

--- app/Validation/OpenCloseTimeRule.php
+++ app/Validation/OpenCloseTimeRule.php
@@ -11,37 +11,26 @@ use Spatie\Permission\Models\Role;
 
 class OpenCloseTimeRule implements Rule
 {
+    protected $dayOfWeek;
+
+    public function __construct($dayOfWeek)
+    {
+        $this->dayOfWeek = $dayOfWeek;
+    }
 
     public function passes($attribute, $value)
     {
-        $openingHours = request()->input('opening_hours');
-        $closingHours=request()->input('opening_hours');
-        $weekDays = request()->input('day_of_week');
+        $index = array_search($this->dayOfWeek, array_column(request()->input('day_of_week'), 0));
 
-
-        if(!isset($weekDays) && isset($openingHours) && isset($closingHours)){
-            return true;
+        if ($index !== false && isset($value[$index]) && isset(request()->input('opening_hours')[$index]) && isset(request()->input('closing_hours')[$index])) {
+            return $value[$index] === request()->input('opening_hours')[$index] && $value[$index] === request()->input('closing_hours')[$index];
         }
 
-        if( $weekDays !== null &&  $openingHours[0] == null ){
-           return false;
-       }
-
-        if($weekDays !== null &&  $openingHours[0] == null ){
-            return false;
-        }
-
-        if( $weekDays == null &&  $closingHours[0] !== null ){
-            return false;
-        }
-
-        if( $weekDays == null && $openingHours[0] !==null ){
-            return false;
-        }
+        return false;
     }
 
     public function message()
     {
-        return 'You Should put valid opening hours and close hours and week of days';
+        return 'The opening and closing hours must be the same for the selected day of the week.';
     }
 }
