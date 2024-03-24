53a9cfabca95f196ed5151222cad0c549aa817dd
Asma Abughaith <asma.abughaith@gmail.com>
2024-03-24 23:57:22 +0300

    fix reviewable table to add s

--- database/migrations/2024_03_21_210404_create_reviewable_table.php
+++ database/migrations/2024_03_21_210404_create_reviewable_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reviewable');
+        Schema::dropIfExists('reviewables');
     }
 };
